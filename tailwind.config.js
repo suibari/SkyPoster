c6440ba5cb64d2e6e512f3dba3d442550aa86df7
suibari <404.not.found.address@gmail.com>
2024-10-14 02:44:32 +0900

    tmp commit, ライブラリをdevに持っていった

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,25 @@
-/** @type {import('tailwindcss').Config} */
-import flowbitePlugin from 'flowbite/plugin'
+import flowbitePlugin from 'flowbite/plugin';
 
+/** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-    extend: {
-      colors: {
-        primary: '#4a90e2',  // 落ち着いたブルー
-        background: '#333333',  // ダーク背景
-        text: '#e0e0e0',  // 落ち着いたテキスト色
-        accent: '#ffcc00',  // アクセントカラー
-      },
+		extend: {
+			colors: {
+				primary: '#4a90e2', // 落ち着いたブルー
+				background: '#333333', // ダーク背景
+				text: '#e0e0e0', // 落ち着いたテキスト色
+				accent: '#ffcc00' // アクセントカラー
+			},
 			fontFamily: {
-        comic: ['"Comic Sans MS"', 'cursive', 'sans-serif'], // カスタムフォント追加
-      },
-    },
-    container: {
-      center: true,
-    },
-  },
+				comic: ['"Comic Sans MS"', 'cursive', 'sans-serif'] // カスタムフォント追加
+			}
+		},
+		container: {
+			center: true
+		}
+	},
 
 	plugins: [require('@tailwindcss/typography'), flowbitePlugin]
 };
