05bd744cf794aacc395c50ef32d1dd85f1bf4847
suibari <404.not.found.address@gmail.com>
2024-10-16 19:03:27 +0900

    symlinkがNGだったので、ビルド時にstaticにコピーするよう修正

--- src/lib/notify.js
+++ src/lib/notify.js
@@ -1,5 +1,5 @@
-import { PUBLIC_VAPID_KEY } from '$env/static/public';
-const workerPath = '/service-worker.js';
+import { PUBLIC_VAPID_KEY, PUBLIC_NODE_ENV } from '$env/static/public';
+const workerPath = (PUBLIC_NODE_ENV === 'development') ? './src/service-worker.js' : '/service-worker.js';
 
 export function urlBase64ToUint8Array(base64String) {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		viteStaticCopy({
+      targets: [
+        {
+          src: 'src/service-worker.js',
+          dest: ''
+        }
+      ]
+    }),
+	],
 	server: {
 		host: true,
 		port: 7860,
