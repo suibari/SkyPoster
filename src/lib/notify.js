db2c8af48701c552db15b0ca1162bec2bb1097d6
suibari <404.not.found.address@gmail.com>
2024-10-16 19:16:20 +0900

    service-workerのパス修正お試し

--- src/lib/notify.js
+++ src/lib/notify.js
@@ -1,5 +1,6 @@
 import { PUBLIC_VAPID_KEY, PUBLIC_NODE_ENV } from '$env/static/public';
-const workerPath = (PUBLIC_NODE_ENV === 'development') ? './src/service-worker.js' : '/service-worker.js';
+import workerUrl from '$src/service-worker.js?url';
+const workerPath = (PUBLIC_NODE_ENV === 'development') ? './src/service-worker.js' : workerUrl;
 
 export function urlBase64ToUint8Array(base64String) {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
