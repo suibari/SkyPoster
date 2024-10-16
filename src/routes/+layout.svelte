575300239627b4d8fda200570a4bf03b033a742b
suibari <404.not.found.address@gmail.com>
2024-10-16 22:19:06 +0900

    erudaをpreview, devに追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,16 @@
 	import '../app.css';
   import { onMount } from 'svelte';
 	import { registerServiceWorker } from '$lib/notify';
+  import { PUBLIC_NODE_ENV } from '$env/static/public';
+  import eruda from 'eruda';
 
 	onMount(() => {
 		registerServiceWorker();
 	});
+
+  if (PUBLIC_NODE_ENV !== 'production') {
+    eruda.init();
+  }
 </script>
 
 <slot></slot>
